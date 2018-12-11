bd967f59b9eb71299d78e8654aacf8ab9c9a9d77
muhammad-dev <ali@eforte.net>
2018-12-11 13:22:55 +0500

    changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hi taimur , how r yous');
+ res.send('heyy ????');
  console.log('server running');
 });
 //listen to port 3000 by default
