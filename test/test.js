2035295deceedcb0f5cac4402c806fb03f914821
muhammad-dev <ali@eforte.net>
2018-12-11 11:22:46 +0500

    fix

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hello world change', function(done) {
+ it('respond with hello world change 1', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world change', done);
+ request(app).get('/').expect('hello world change 1', done);
  });
 });
